9cfdfafdab34f7332c3e1cf0be5eb7b655167353
Patricio <promeroc11@gmail.com>
2025-05-19 11:52:21 -0400

    Six Update: Instructions for evaluation and minor changes

---
+++ readme.txt
@@ -0,0 +1,13 @@
+Urls to test
+
+Registration
+api/registration/
+
+Token Obtain
+api/api-token-auth/
+
+Menu Items
+api/menu-items/
+
+Bookings
+api/bookings/tables
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -6,7 +6,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('', index, name='index'),
     path('menu-items/', MenuItemView.as_view(), name='menu-items'),
-    path("menu-items/<int:pk>", SingleMenuItemView.as_view(), name="menu-item-detail"),
+    path("menu-items/<int:pk>/", SingleMenuItemView.as_view(), name="menu-item-detail"),
     path('api-token-auth/', obtain_auth_token, name='user-login'),
     path('registration/', RegisterUserView.as_view(), name='user-register')
 ]
