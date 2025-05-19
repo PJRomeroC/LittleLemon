d2c05f3dabf1586dc2fec0edc9d87e072ea9fda8
Patricio <promeroc11@gmail.com>
2025-05-19 06:28:52 -0400

    Fourth Update: Add Djoser and Token Auth

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,9 +1,11 @@
 #define URL route for index() view
 from django.urls import path
-from .views import *
+from .views import MenuItemView, SingleMenuItemView, index
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', index, name='index'),
     path('menu/', MenuItemView.as_view(), name='menu-items'),
     path("menu/<int:pk>", SingleMenuItemView.as_view(), name="menu-item-detail"),
+    path('api-token-auth/', obtain_auth_token),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
-from .models import *
-from .serializers import *
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
 # Create your views here.
 def index(request):
