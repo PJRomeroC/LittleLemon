481cee2aa6688ab618d674b62351df769f31143f
Patricio <promeroc11@gmail.com>
2025-05-19 04:51:25 -0400

    Third Update: Serializers and Views Update

--- restaurant/models.py
+++ restaurant/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Booking(models.Model):
     name = models.CharField(max_length=255)
     no_of_guest = models.SmallIntegerField()
-    bookingDate = models.DateTimeField()
+    bookingdate = models.DateTimeField()
 
     def __str__(self):
         return self.name
---
+++ restaurant/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import *
+
+class MenuSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Menu
+        fields = ['id','title','price','inventory']
+
+class BookingSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Booking
+        fields = ['id','no_of_guest','bookingdate']
